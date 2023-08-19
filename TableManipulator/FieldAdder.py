d3420c00619a92b5ed60255bb2831cad2de89faa
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-08-19 10:59:58 +0300

    fix py script adding is complete field

--- TableManipulator/FieldAdder.py
+++ TableManipulator/FieldAdder.py
@@ -1,3 +1,6 @@
+# Add 'Is Complete' field checking completion of inflation data through the years.
+# True if all data is present and False if there is even single NULL data.
+
 import pandas as pd
 
 # Read the CSV file into a pandas DataFrame
@@ -9,10 +12,10 @@ year_columns = [str(year) for year in range(1960, 2023)]
 # Check for each record if all year columns are not null
 data['Is Complete'] = data[year_columns].notnull().all(axis=1)
 
-# Reorder columns to have 'Is Complete' after 'Indicator Code'
-column_order = list(data.columns)
-column_order.insert(column_order.index('Indicator Code') + 1, 'Is Complete')
+# Reorder columns with 'Is Complete' after 'Indicator Code'
+column_order = ['Country Name', 'Country Code', 'Indicator Name', 'Indicator Code', 'Is Complete'] + year_columns
 data = data[column_order]
 
 # Save the updated DataFrame back to a CSV file
 data.to_csv('output_file.csv', index=False)
+
