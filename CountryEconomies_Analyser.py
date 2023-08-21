84684c70e8b49cff4290a3befbef422bafbf7dfb
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2023-08-21 23:14:47 +0300

    add report about gdp-inflation graph

--- CountryEconomies_Analyser.py
+++ CountryEconomies_Analyser.py
@@ -21,9 +21,8 @@ sql_query = f"""
     SELECT
         gdp.country_name,
         {year_columns}
-    FROM gdp_per_capita AS gdp
-    WHERE country_name IN ('Turkiye', 'European Union',
-      'Middle East & North Africa');
+    FROM inflation_consumer AS gdp
+    WHERE country_name IN ('Turkiye', 'Germany');
 """
 
 # Fetch data from the gdp_countries table
@@ -36,12 +35,11 @@ inf_query = f"""
         inf.country_name,
         {year_columns}
     FROM gdp_per_capita_growth AS inf
-    WHERE country_name IN ('Turkiye', 'European Union',
-      'Middle East & North Africa');
+    WHERE country_name IN ('Turkiye', 'Germany');
 """
-# WHERE inf.country_name IN ('Turkiye', 'World', 
-#     'Middle East & North Africa', 'European Union')
-#     ORDER BY inf.country_code DESC;
+#, 'European Union',
+    #  'Middle East & North Africa'
+
 cur.execute(inf_query)
 inf_data = cur.fetchall()
 
@@ -65,17 +63,30 @@ for row in inf_data:
     inf_values.append(row[1:])
     inf_countries.append(country_name)
 
+# Calculate the difference between GDP and inflation
+diff_values = []
+
+for gdp_row, inf_row in zip(gdp_values, inf_values):
+    diff_row = []
+    for gdp, inf in zip(gdp_row, inf_row):
+        if gdp is not None and inf is not None:
+            diff_row.append(inf - gdp)
+        else:
+            diff_row.append(None)
+    diff_values.append(diff_row)
+
+
 # Create a line plot
-fig, axes = plt.subplots(2, 1, figsize=(10, 12))
+fig, axes = plt.subplots(3, 1, figsize=(10, 18))
 
 # Plot GDP data in the first subplot
 for country, gdp in zip(gdp_countries, gdp_values):
     axes[0].plot(years, gdp, marker='o', label=f"{country} GDP Per Capita")
 
-axes[0].set_title("Yearly GDP Per Capita Data")
+axes[0].set_title("Yearly Consumer Prices Inflation Graph")
 axes[0].set_xlabel("Year")
-axes[0].set_ylabel("GDP Per Capita Value")
-axes[0].set_yscale("log")
+axes[0].set_ylabel("GDP Consumer Prices Inflation Ratio")
+# axes[0].set_yscale("log")
 axes[0].grid(True)
 axes[0].legend()
 
@@ -83,12 +94,22 @@ axes[0].legend()
 for country, inf in zip(inf_countries, inf_values):
     axes[1].plot(years, inf, marker='o', label=f"{country} GDP PC Growth")
 
-axes[1].set_title("Yearly GDP PC Growth Data")
+axes[1].set_title("Yearly GDP PC Growth Graph")
 axes[1].set_xlabel("Year")
-axes[1].set_ylabel("GDP PC Growth Value")
+axes[1].set_ylabel("GDP PC Growth Ratio")
 # axes[1].set_yscale("log")
 axes[1].grid(True)
 axes[1].legend()
+
+for country, diff in zip(gdp_countries, diff_values):
+    axes[2].plot(years, diff, marker='o', label=f"{country} GDP - Inflation")
+
+    
+axes[2].set_title("Yearly GDP - Inflation Graph")
+axes[2].set_xlabel("Year")
+axes[2].set_ylabel("GDP - Inflation Ratio")
+axes[2].grid(True)
+axes[2].legend()
 
 # Adjust layout
 plt.tight_layout()
